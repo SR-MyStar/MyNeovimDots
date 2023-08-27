96848adfb208e1c977b29cf5a13efe5dfc0a9123
star <yanwenz551@gmail.com>
2023-08-28 01:08:46 +0800

    要提交的变更： 	修改：     visual-multi.lua

--- lua/plugins/visual-multi.lua
+++ lua/plugins/visual-multi.lua
@@ -10,16 +10,16 @@ return {
       ["Select Cursor Down"] = "<M-C-S-Down>",
       ["Select Cursor Up"] = "<M-C-S-Up>",
 
-      ["Select All"] = "<leader><leader>A",
-      ["Start Regex Search"] = "<leader><leader>/",
+      ["Select All"] = "<leader>\\A",
+      ["Start Regex Search"] = "<leader>\\/",
       ["Add Cursor Down"] = "<C-S-Down>",
       ["Add Cursor Up"] = "<C-S-Up>",
-      ["Add Cursor At Pos"] = "<leader><leader><leader>",
-      ["Visual Regex"] = "<leader><leader>/",
-      ["Visual All"] = "<leader><leader>A",
-      ["Visual Add"] = "<leader><leader>a",
-      ["Visual Find"] = "<leader><leader>f",
-      ["Visual Cursors"] = "<leader><leader>c",
+      ["Add Cursor At Pos"] = "<leader>\\<leader>",
+      ["Visual Regex"] = "<leader>\\/",
+      ["Visual All"] = "<leader>\\A",
+      ["Visual Add"] = "<leader>\\a",
+      ["Visual Find"] = "<leader>\\f",
+      ["Visual Cursors"] = "<leader>\\c",
     }
     vim.g.VM_Mono_hl = "FlashLabel"
     vim.g.VM_Extend_hl = "FlashCurrent"
