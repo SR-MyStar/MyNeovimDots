045e81c68156a30100a4a1892fe81131f261b417
纳西妲 · Nahida <yanwenz551@gmail.com>
2024-01-07 18:57:50 +0800

    fix(yazi): yazi can't open file

--- lua/config/keymaps/yazi.lua
+++ lua/config/keymaps/yazi.lua
@@ -4,9 +4,10 @@ return {
   keys = {
     e = {
       function()
+        local tempname = vim.fn.tempname()
         Util.terminal({
           "yazi",
-          "--chooser-file=/tmp/temp-yazi-chooser-file-temp.txt",
+          "--chooser-file=" .. tempname,
         }, {
           cwd = Util.root(),
           esc_esc = false,
@@ -16,16 +17,12 @@ return {
           title_pos = "center",
           size = { height = 0.8, width = 0.9 },
           on_exit = function()
-            if
-              vim.loop.fs_stat("/tmp/temp-yazi-chooser-file-temp.txt")
-              ~= nil
-            then
-              local selected_file_paths =
-                vim.fn.readfile("/tmp/temp-yazi-chooser-file-temp.txt")
-              vim.fn.delete("/tmp/temp-yazi-chooser-file-temp.txt")
-              for _, selected_file_path in pairs(selected_file_paths) do
-                vim.api.nvim_command("e " .. selected_file_path)
+            ---@diagnostic disable-next-line: param-type-mismatch
+            if vim.loop.fs_statfs(tempname) then
+              for _, file in pairs(vim.fn.readfile(tempname)) do
+                vim.api.nvim_command("e " .. file)
               end
+              vim.fn.delete(tempname)
             end
           end,
         })
@@ -34,9 +31,10 @@ return {
     },
     E = {
       function()
+        local tempname = vim.fn.tempname()
         Util.terminal({
           "yazi",
-          "--chooser-file=/tmp/temp-yazi-chooser-file-temp.txt",
+          "--chooser-file=" .. tempname,
         }, {
           esc_esc = false,
           ctrl_hjkl = false,
@@ -45,14 +43,12 @@ return {
           title_pos = "center",
           size = { height = 0.8, width = 0.9 },
           on_exit = function()
-            if
-              vim.loop.fs_stat("/tmp/temp-yazi-chooser-file-temp.txt")
-              ~= nil
-            then
-              local filepath = "/tmp/temp-yazi-chooser-file-temp.txt"
-              local selected_file_path = vim.fn.readfile(filepath)[1]
-              vim.fn.delete("/tmp/temp-yazi-chooser-file-temp.txt")
-              vim.api.nvim_command("e " .. selected_file_path)
+            ---@diagnostic disable-next-line: param-type-mismatch
+            if vim.loop.fs_statfs(tempname) then
+              for _, file in pairs(vim.fn.readfile(tempname)) do
+                vim.api.nvim_command("e " .. file)
+              end
+              vim.fn.delete(tempname)
             end
           end,
         })
